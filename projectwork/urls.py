f4d89c139c6d38d806687dcfbb1eecb8f7b816b9
steven <steven@nenyah.com>
2018-12-13 10:30:58 +0800

    delete useless file

--- projectwork/urls.py
+++ projectwork/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.urls import path
 from django.contrib import admin
 from django.conf.urls.static import static
-from django.contrib.auth.views import logout
+from django.contrib.auth import views as auth_views
 from django.conf import settings
 from workapp.views import alteruser, appointment, detail, index, product_list, login_view, register, userinfo, forgetpass, test
 
@@ -29,7 +29,8 @@ urlpatterns = [
     path('list/', product_list, name='productlist'),
     # path('list/<int:area>/', product_list, name='product_list'),
     path('login/', login_view, name='login'),
-    path('logout/', logout, {'next_page': '/index/'}, name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(),
+         {'next_page': '/index/'}, name='logout'),
     path('register/', register, name='register'),
     path('userinfo/', userinfo, name='userinfo'),
     path('forgetpass/', forgetpass, name='forgetpass'),
@@ -41,5 +42,7 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
