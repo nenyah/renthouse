218da514d859b2608c74f63f6a32246f9f4c8457
steven <steven@nenyah.com>
2019-03-12 11:05:53 +0800

    move sqlite database outside of main source tree

--- source/renthouse/wsgi.py
+++ source/renthouse/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projectwork.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "renthouse.settings")
 
 application = get_wsgi_application()
